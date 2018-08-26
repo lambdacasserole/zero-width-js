24205ba9ddbb38a4035eb533aba23975cbd42f9e
Saul Johnson <saul.a.johnson@gmail.com>
2018-08-26 16:26:17 +0100

    Make sure detector reads all input

--- src/detection/detector.py
+++ src/detection/detector.py
@@ -1,13 +1,18 @@
+import sys
+
+# Read standard input to end.
+document = ' '.join(sys.stdin.readlines())
+
 # Detect any forbidden characters in input.
-payload = input()
 forbidden = ['\u200b', '\u200c', '\u200d']
 alert = False
-for char in payload:
-    if char in forbidden:
-        alert = True
+for char in document:
+  if char in forbidden:
+    alert = True
 
 # Show message depending on whether or not zero-width characters were found.
 if alert:
-    print("Document contains zero-width characters!")
+    print('Document contains zero-width characters!')
 else:
-    print("Document does not contain zero-width characters!")
+    print('Document does not contain zero-width characters!')
+    
