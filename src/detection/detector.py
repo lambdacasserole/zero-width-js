54585b203976a011291ecf85eda8b7370a8a37e8
Saul Johnson <saul.a.johnson@gmail.com>
2018-08-26 13:40:17 +0100

    Add detector script

---
+++ src/detection/detector.py
@@ -0,0 +1,13 @@
+# Encode input ASCII as binary.
+payload = input()
+forbidden = ['\u200b', '\u200c', '\u200d']
+alert = False
+for char in payload:
+    if char in forbidden:
+        alert = True
+
+# Show message depending on whether or not zero-width characters were found.
+if alert:
+    print("Document contains zero-width characters!")
+else:
+    print("Document does not contain zero-width characters!")
